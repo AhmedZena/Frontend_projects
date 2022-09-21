f6eb8a132aa88889fb2d01e7f0ad6078f324607c
Ahmed Zena <ahmedzena53@gmail.com>
2022-09-21 17:39:35 +0200

    js code of the iphone

---
+++ Iphone site/main.js
@@ -0,0 +1,9 @@
+//call all pictures that will change themes on click
+let redTheme = document.getElementById("redTheme");
+let blackTheme = document.getElementById("blackTheme");
+let greyTheme = document.getElementById("greyTheme");
+console.log(redTheme);
+
+redTheme.addEventListener("click", function () {
+  console.log("red theme");
+});
